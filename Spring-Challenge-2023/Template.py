6e0256f9acdbc5100fada814e4905fdfa73f897d
Crackson4 <zachariahhart18@gmail.com>
2023-05-26 00:57:20 +1000

    Tidied up Template.py Made all variables accessible in code Renamed files to be more concise Added a template for a function to determine the shortest path from base to a given cell

--- Spring-Challenge-2023/Template.py
+++ Spring-Challenge-2023/Template.py
@@ -1,27 +1,28 @@
-number_of_cells = int(input())  # amount of hexagonal cells in this map
-for i in range(number_of_cells):
+import math
+
+cells_num = int(input())  # no. of cells
+
+cell_list = []  # list of tuples with cell info
+
+for i in range(cells_num):
     # _type: 0 for empty, 1 for eggs, 2 for crystal
     # initial_resources: the initial amount of eggs/crystals on this cell
     # neigh_0: the index of the neighbouring cell for each direction
-    _type, initial_resources, neigh_0, neigh_1, neigh_2, neigh_3, neigh_4, neigh_5 = [
-        int(j) for j in input().split()
-    ]
-number_of_bases = int(input())
-for i in input().split():
-    my_base_index = int(i)
-for i in input().split():
-    opp_base_index = int(i)
+    cell_list.append(tuple([int(j) for j in input().split()]))
+
+bases_num = int(input())  # no. of bases
+
+my_base_indexes = [int(i) for i in input().split()]  # indexes of our bases
+opp_base_indexes = [int(i) for i in input().split()]  # indexes of opponent bases
 
 # game loop
 while True:
-    for i in range(number_of_cells):
+    for i in range(cells_num):
         # resources: the current amount of eggs/crystals on this cell
         # my_ants: the amount of your ants on this cell
         # opp_ants: the amount of opponent ants on this cell
         resources, my_ants, opp_ants = [int(j) for j in input().split()]
 
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+        print("Commands")
 
     # WAIT | LINE <sourceIdx> <targetIdx> <strength> | BEACON <cellIdx> <strength> | MESSAGE <text>
-    print("WAIT")
--- Spring-Challenge-2023/Wood2.py
+++ Spring-Challenge-2023/Wood2.py
@@ -1,24 +1,32 @@
-import sys
 import math
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
+cells_num = int(input())  # amount of hexagonal cells in this map
 
-number_of_cells = int(input())  # amount of hexagonal cells in this map
-for i in range(number_of_cells):
+for i in range(cells_num):
     # _type: 0 for empty, 1 for eggs, 2 for crystal
     # initial_resources: the initial amount of eggs/crystals on this cell
     # neigh_0: the index of the neighbouring cell for each direction
-    _type, initial_resources, neigh_0, neigh_1, neigh_2, neigh_3, neigh_4, neigh_5 = [int(j) for j in input().split()]
+    type, initial_resources, neigh_0, neigh_1, neigh_2, neigh_3, neigh_4, neigh_5 = [
+        int(j) for j in input().split()
+    ]
 number_of_bases = int(input())
 for i in input().split():
     my_base_index = int(i)
 for i in input().split():
     opp_base_index = int(i)
 
+
+def cell_dist(cell):
+    # finds the shortest path from the base to a cell
+    # iterates through possible paths
+
+    paths = []
+    return min([len(path) for path in paths])
+
+
 # game loop
 while True:
-    for i in range(number_of_cells):
+    for i in range(cells_num):
         # resources: the current amount of eggs/crystals on this cell
         # my_ants: the amount of your ants on this cell
         # opp_ants: the amount of opponent ants on this cell
@@ -26,7 +34,6 @@ while True:
 
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
-
 
     # WAIT | LINE <sourceIdx> <targetIdx> <strength> | BEACON <cellIdx> <strength> | MESSAGE <text>
     print("WAIT")
